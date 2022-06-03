3d89db2b0e1f4c5cb6fbcd97a0eb9a20b26dd451
Imanol Gutierrez <developer@imanol.org>
2022-06-03 14:25:01 +0200

    Create lexer

---
+++ src/Lexer.cc
@@ -0,0 +1,65 @@
+
+/**
+ * Token values
+ */
+enum Token {
+    // EOF token
+    tok_eof = -1,
+
+    // comands
+    tok_def = -2,
+    tok_extern = -3,
+
+    // primary
+    tok_identifier = -4,
+    tok_number = -5
+};
+
+static std::string IdentifierString;  // value if token is tok_identifier
+static double NumVal;           // value if token is tok_number
+
+/**
+ * This gettok() function recognizes the text and returns the token value of the parsed input
+ * @return token value
+ */
+static int gettok() {
+    static int LastChar = ' ';
+
+    // ignore space chars
+    while (isspace(LastChar))
+        LastChar = getchar();
+
+    // recognize identifiers and keywords
+    if (isalpha(LastChar)) {    // [a-zA-Z][a-zA-Z0-9]*
+        IdentifierString = LastChar;
+        while (isalnum((LastChar = getchar())))
+            IdentifierString += LastChar;
+        if (IdentifierString == "def")
+            return tok_def;
+        if (IdentifierString == "extern")
+            return tok_extern;
+        return tok_identifier;
+    }
+
+    // recognize numbers
+    if (isdigit(LastChar) || LastChar == '.') { // [0-9.]+
+        std::string NumString;
+        do {
+            NumString += LastChar;
+            LastChar = getchar();
+        } while (isdigit(LastChar) || LastChar == '.');
+
+        NumVal = strtod(NumString.c_str(), 0);
+        return tok_number;
+    }
+
+    // recognize comments
+    if (LastChar == '#') {
+        do {
+            LastChar = getchar();
+        } while (LastChar != EOF && LastChar != '\n' && LastChar != '\r');
+
+        if (LastChar != EOF)
+            return gettok();
+    }
+}

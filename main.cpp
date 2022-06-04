8a622a8e1e795cb4dfc58346b2bc2d48b09bf017
Imanol Gutierrez <developer@imanol.org>
2022-06-04 09:30:08 +0200

    Start Parser

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 
-#include "src/parser/Lexer.cc"
+#include "src/parser/Parser.cc"
 
 using namespace  std;
 
 int main(int argc, char **argv) {
-    while (true) {
-        int strtok = gettok();
-        cout << strtok << "\tNum: " << NumVal << "\tStr: " << IdentifierString  << endl;
-    }
+    MainLoop();
 }
 

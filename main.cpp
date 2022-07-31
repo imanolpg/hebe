fbf18fa9c953a0668c6df512d7858f50a1f48bff
Imanol Gutierrez <developer@imanol.org>
2022-07-31 17:54:59 +0200

    Add JIT

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 
 #include "src/Parser.h"
+#include "src/JITDriver.h"
+#include "src/CodeGeneration.h"
 
 
 int main(int argc, char **argv) {
+
+    InitializeModule();
+
     MainLoop();
+
+    // print the generated code
+    TheModule->print(llvm::errs(), nullptr);
+
+    return 0;
 }
 
---
+++ src/JITDriver.h
@@ -0,0 +1,16 @@
+/*!
+ * @author Imanol
+ * @date 31/7/22
+ * @project hebe
+ */
+
+#ifndef HEBE_JITDRIVER_H
+#define HEBE_JITDRIVER_H
+
+void InitializeModule();
+
+void HandleDefinition();
+
+void HandleExtern();
+
+#endif //HEBE_JITDRIVER_H

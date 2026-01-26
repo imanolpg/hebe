77388f2f42f24bc0d00e555a558d5fb77b64f2af
imanolpg <imanol@imanols-MacBook-Pro.local>
2026-01-26 20:53:56 +0100

    Pass clang-format

--- src/logging.cpp
+++ src/logging.cpp
@@ -1,20 +1,21 @@
 #include "logging.h"
-#include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/sinks/basic_file_sink.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 namespace {
-  std::shared_ptr<spdlog::logger> g_logger;
+std::shared_ptr<spdlog::logger> g_logger;
 }
 
 namespace logsys {
 
 void init() {
-  if (g_logger) return;
+  if (g_logger)
+    return;
 
   auto console = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-  auto file    = std::make_shared<spdlog::sinks::basic_file_sink_mt>("hebe.log", true);
+  auto file = std::make_shared<spdlog::sinks::basic_file_sink_mt>("hebe.log", true);
 
-  std::vector<spdlog::sink_ptr> sinks { console, file };
+  std::vector<spdlog::sink_ptr> sinks{console, file};
   g_logger = std::make_shared<spdlog::logger>("hebe", sinks.begin(), sinks.end());
 
   // default level: info (use debug while developing)
